d9b172c6796c2f17ec299b4378badc422c894ce4
Sergey Stupnikov <sergey.stupnikov.59@yandex.ru>
2022-09-01 00:10:52 +0500

    в процессе

---
+++ MovieQuizTests/MoviesLoaderTests.swift
@@ -0,0 +1,109 @@
+import Foundation
+import XCTest // не забывайте импортировать фреймворк для тестирования
+@testable import MovieQuiz // импортируем приложение для тестирования
+
+
+struct StubNetworkClient: NetworkRouting {
+
+    enum TestError: Error { // тестовая ошибка
+    case test
+    }
+
+    let emulateError: Bool // этот параметр нужен, чтобы заглушка эмулировала либо ошибку сети, либо успешный ответ
+
+    func fetch(url: URL, handler: @escaping (Result<Data, Error>) -> Void) {
+        if emulateError {
+            handler(.failure(TestError.test))
+        } else {
+            handler(.success(expectedResponse))
+        }
+    }
+
+     var expectedResponse: Data {
+        """
+        {
+           "errorMessage" : "",
+           "items" : [
+              {
+                 "crew" : "Dan Trachtenberg (dir.), Amber Midthunder, Dakota Beavers",
+                 "fullTitle" : "Prey (2022)",
+                 "id" : "tt11866324",
+                 "imDbRating" : "7.2",
+                 "imDbRatingCount" : "93332",
+                 "image" : "https://m.media-amazon.com/images/M/MV5BMDBlMDYxMDktOTUxMS00MjcxLWE2YjQtNjNhMjNmN2Y3ZDA1XkEyXkFqcGdeQXVyMTM1MTE1NDMx._V1_Ratio0.6716_AL_.jpg",
+                 "rank" : "1",
+                 "rankUpDown" : "+23",
+                 "title" : "Prey",
+                 "year" : "2022"
+              },
+              {
+                 "crew" : "Anthony Russo (dir.), Ryan Gosling, Chris Evans",
+                 "fullTitle" : "The Gray Man (2022)",
+                 "id" : "tt1649418",
+                 "imDbRating" : "6.5",
+                 "imDbRatingCount" : "132890",
+                 "image" : "https://m.media-amazon.com/images/M/MV5BOWY4MmFiY2QtMzE1YS00NTg1LWIwOTQtYTI4ZGUzNWIxNTVmXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_Ratio0.6716_AL_.jpg",
+                 "rank" : "2",
+                 "rankUpDown" : "-1",
+                 "title" : "The Gray Man",
+                 "year" : "2022"
+              }
+            ]
+          }
+        """.data(using: .utf8) ?? Data()
+    }
+}
+
+
+
+
+class MoviesLoaderTests: XCTestCase {
+    func testSuccessLoading() throws {
+        // Given
+        let stubNetworkClient = StubNetworkClient(emulateError: false)
+        let loader: MoviesLoading = MoviesLoader(networkClient: stubNetworkClient)
+//        print(" 0 0 0 0 0  0 0 0 0 0 0    0 0 0 0 ")
+//        print("expectedResponse", stubNetworkClient.expectedResponse)
+        // When
+
+        // так как функция загрузки фильмов — асинхронная, нужно ожидание
+        let expectation = expectation(description: "Loading expectation")
+
+        loader.loadMovies { result in
+            // Then
+            switch result {
+            case .success(let movies):
+                // сравниваем данные с тем, что мы предполагали
+                XCTAssertEqual(movies.items.count, 2)
+                expectation.fulfill()
+            case .failure:
+                // мы не ожидаем, что пришла ошибка; если она появится, надо будет провалить тест
+                XCTFail("Unexpected failure") // эта функция проваливает тест
+            }
+        }
+        waitForExpectations(timeout: 3)
+    }
+
+    func testFailureLoading() throws {
+        // Given
+        let stubNetwrokClient = StubNetworkClient(emulateError: true) // говорим, что хотим эмулировать ошибку
+        let loader = MoviesLoader(networkClient: stubNetwrokClient)
+
+        // When
+        let expectation = expectation(description: "Loading expectation")
+
+        loader.loadMovies { result in
+            // Then
+            switch result {
+            case .failure(let error):
+                XCTAssertNotNil(error)
+                expectation.fulfill()
+            case .success(_):
+                XCTFail("Unexpected failure")
+            }
+        }
+
+        waitForExpectations(timeout: 1)
+    }
+}
+
--- MoviesLoader.swift
+++ MoviesLoader.swift
@@ -6,8 +6,12 @@ protocol MoviesLoading {
 
 struct MoviesLoader: MoviesLoading {
     // MARK: - NetworkClien
-    private let networkClient = NetworkClient()
+//    private let networkClient = NetworkClient()
+    private let networkClient: NetworkRouting
 
+    init(networkClient: NetworkRouting = NetworkClient()) {
+        self.networkClient = networkClient
+    }
     // MARK: - URL
     private var mostPopularMoviesUrl: URL {
          guard let url = URL(string: "https://imdb-api.com/en/API/MostPopularMovies/k_kiwxbi4y") else {
